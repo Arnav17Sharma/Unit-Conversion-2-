cae7e70e654be0c0c8e3a97917c82ea39b992838
Arnav17Sharma <58596929+Arnav17Sharma@users.noreply.github.com>
2020-01-02 12:07:40 +0530

    Edited GCI-2.py

--- GCI-App(2)/GCI-2.py
+++ GCI-App(2)/GCI-2.py
@@ -1,40 +1,60 @@
-num = int(input("Enter magnitude: "))
-source = input("Enter source unit: ")
-target = input("Enter target unit: ")
-d = {'milimeter':[1,'len'],
-    'centimeter':[10,'len'],
-    'meter':[1000,'len'],
-    'kilometer':[1000000,'len'],
-    'inch':[25.4,'len'],
-    'feet':[304.8,'len'],
-    'yard':[914.4,'len'],
-    'mile':[1609344,'len'],
-    'second':[1,'time'],
-    "minute":[60,'time'],
-    "hour":[60*60,'time'],
-    "day":[24*60*60,'time'],
-    "week":[7*24*60*60,'time'],
-    "year":[365*24*60*60,'time'],
-    'cm/s':[1,"speed"],
-    'cm/min':[60,"speed"],
-    'cm/hr': [60*60,"speed"],
-    'm/s':[1/100,"speed"],
-    'm/min':[60/100,"speed"],
-    'm/hr': [60*60/100,"speed"],
-    'km/s':[1/100000,"speed"],
-    'km/min':[60/100000,"speed"],
-    'km/hr': [60*60/100000,"speed"]
-     }
-if source in d.keys():
-    if target in d.keys():
-        if d[source][1] == d[target][1]:
-            if d[source][1] == 'speed':
-                print((num)*(d[target][0]/d[source][0]),target)
-            else:
-                print((num)*(d[source][0]/d[target][0]),target)
-        else:
-            print("ENTER COMPARABLE PARAMETERS...")
+magnitude=int(input("Enter magnitude: "))
+source=input("Enter source unit: ")
+target=input("Enter target unit: ")
+l=[]
+l1=['mm','cm','m','km','second','minute','hour','day','week','year','cm/s','cm/min','cm/hr','m/s','m/min','m/hr','km/s','km/,min','km/hr']
+value=[]
+l.append(target)
+d={
+"mm":"1 mm",
+"cm":"10 mm",
+"m":"100 cm",
+"km":"1000 m",
+"second":"1 second",
+"minute":"60 second",
+"hour":"60 minute",
+"day":"24 hour",
+"week":"7 day",
+"year":"52 week",
+"cm/s":"1 cm/s",
+"cm/min":"0.016666666666666666 cm/s",
+"cm/hr":"0.016666666666666666 cm/min",
+"m/s":"360000 cm/hr",
+"m/min":"0.016666666666666666 m/s",
+"m/hr":"0.016666666666666666 m/min",
+"km/s":"3600000 m/hr",
+"km/min":"0.016666666666666666 km/s",
+"km/hr":"0.016666666666666666 km/min"
+}
+a=""
+if 0<=l1.index(source)<=3 and 0<=l1.index(target)<=3 or 4<=l1.index(source)<=9 and 4<=l1.index(target)<=9 or 10<=l1.index(source)<=18 and 10<=l1.index(target)<=18:
+    if l1.index(source)<l1.index(target):
+        while target!=source:
+            for i in d.keys():
+                if str(i)==target:
+                    target = d[target]
+                    a=target.split(" ")
+                    target=a[-1]
+                    l.append(a[-1])
+                    value.append(float(a[0]))
+        pro=1
+        for i in value:
+            pro*=i
+        print(magnitude/pro,target)
+    elif l1.index(source)==l1.index(target):
+        print(magnitude,target)
     else:
-        print("INVALID INPUT")
+        while target!=source:
+            for i in d.keys():
+                if str(i)==source:
+                    source = d[source]
+                    a=source.split(" ")
+                    source=a[-1]
+                    l.append(a[-1])
+                    value.append(float(a[0]))
+        pro=1
+        for i in value:
+            pro*=i
+        print(magnitude*pro,target)
 else:
     print("INVALID INPUT")
